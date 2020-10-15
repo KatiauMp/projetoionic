6ea2267d4cc4f8c30abe354a025c861254eb9319
KatiauMp <marcos65.senac@gmail.com>
2020-10-15 16:40:00 -0300

    Meu firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase : {
-  apiKey: "AIzaSyD-a5_iWdNDRTHBo81MP0ukiMlCTjkbQbI",
-  authDomain: "senac-20195.firebaseapp.com",
-  databaseURL: "https://senac-20195.firebaseio.com",
-  projectId: "senac-20195",
-  storageBucket: "senac-20195.appspot.com",
-  messagingSenderId: "604562411891",
-  appId: "1:604562411891:web:32865d9dcb1fbe2d3ff506"
+    apiKey: "AIzaSyC3PNWf7R9Bb9jm0ciyy3O6_tS84MWBGtI",
+    authDomain: "projeto-integrador-5c468.firebaseapp.com",
+    databaseURL: "https://projeto-integrador-5c468.firebaseio.com",
+    projectId: "projeto-integrador-5c468",
+    storageBucket: "projeto-integrador-5c468.appspot.com",
+    messagingSenderId: "907647024961",
+    appId: "1:907647024961:web:61fd630b056bf93cee2bc0"
   }
   }; 
